ed743e98551637207cc88542abb12e8a2ef56c57
bhatiya1995 <bhatiya1995@gmail.com>
2022-08-23 20:49:22 +0530

    changed condition

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -208,7 +208,7 @@ class CampaignController extends Controller
                                 \Log::info("adui and wininng bid nemei");
                                 $message = "Hurry Up! Your bid of {$words[1]} is not the winning bid at the moment. Now Lowest bid range is {$range[0]} - {$range[1]}. You have {$availabelBids} more free bid(s) for today";
                                 $this->sendSmsForOne($senderAddress, $message);
-                            }elseif($range[0]<$words[1] or $words[1]<= $range[1]){
+                            }elseif($range[0]<$words[1] and $words[1]<= $range[1]){
                                 $message = "You have {$availabelBids} more free bid(s) for today";
                                 $this->sendSmsForOne($senderAddress, $message);
                             }elseif($words[1] = $winningBid){
