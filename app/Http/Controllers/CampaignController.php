85665c2f23f3505acd26c003444347a0c7a76e85
bhatiya1995 <bhatiya1995@gmail.com>
2022-09-07 19:03:15 +0530

    added sleep method

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -389,6 +389,8 @@ class CampaignController extends Controller
 
     
                         if($campaign != null){
+                            sleep(300);
+                            \Log::info('sleep eka iwarai'); 
                             $message = $campaign->welcome_msg;
                             $this->sendSmsForOne($msisdn, $message);
                         }
